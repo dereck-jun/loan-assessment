6abfad1075c7e4841078a38c39dda31e408222f3
dereck-jun <yjpark0805@naver.com>
2025-07-24 18:20:20 +0900

    Initial commit

---
+++ src/test/java/com/loan/assessment/LoanAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
